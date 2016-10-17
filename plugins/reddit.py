7235d2ef790074a267d4f9d06e2f6e0cbbd31c00
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2016-10-17 00:21:05 -0400

    Fixed a bunch of stuff with subreddit monitoring

--- plugins/reddit.py
+++ plugins/reddit.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-from irc3.plugins.command import command
-import irc3
 import concurrent.futures
 import logging
 import asyncio
-import functools
-import time
-import praw
 import re
+import time
+
+import irc3
+from irc3.plugins.command import command
+import aiohttp
+import praw
+from bidict import bidict
 
 @irc3.plugin
 class Reddit(object):
@@ -19,20 +21,30 @@ class Reddit(object):
     link_template = "\x02r/{sub}\x02 \x037\x02|\x02\x03 {title} - {upvotes} votes \x037\x02|\x02\x03 {num_comments} comments{nsfw}"
     subreddit_generators = {}
     link_regex = re.compile("(?P<link>https?://(?:redd\.it/|w{3}?\.reddit.com/r/\w+/comments/)(?P<link_id>\w+)(?:/\w+/(?P<comment_id>\w+))?)")
-    time_map = { 'hour' : 'in the past hour',
-                      'day'  : ' in the past day',
-                      'week' : ' in the past week',
-                      'month': ' in the past month',
-                      'year' : ' in the past year',
-                      'all'  : ' of all time' }
-    headers = { 'User-Agent' : user_agent }
+    time_map = {'hour' : 'in the past hour',
+                'day'  : ' in the past day',
+                'week' : ' in the past week',
+                'month': ' in the past month',
+                'year' : ' in the past year',
+                'all'  : ' of all time'}
+    headers = {}
+    chan_map = {} # multireddit => list of channels (could be more than one, you never know)
+    generator_map = bidict() # multireddit <=> generator
+    sub_map = {} # subreddit => list of multireddits it's in
+    time_map = {} # multireddit => latest post time
+
 
 
     def __init__(self, bot):
+        def add_to_dict(map, key, value):
+            if key not in map.keys():
+                map[key] = []
+            map[key].append(value)
+
         self.bot = bot
         module = self.__class__.__module__
 
-        self.praw = praw.Reddit(self.user_agent)
+        self.praw = praw.Reddit(user_agent=self.user_agent, site_name='my_reddit')
         self.log = logging.getLogger(module)
 
         if hasattr(self.bot, 'session'):
@@ -44,22 +56,27 @@ class Reddit(object):
         loop.slow_callback_duration = 0.01
         self.config = config = bot.config.get(module, {})
         if not config:
-            self.log.info("No reddit rotators to init!")
             return
 
         del config['hash']
-        if len(config.keys()) > 1:
-            print(config)
+
+        if len(config.keys()) > 0:
             workers = len(config.keys()) + 1
             self.executor = concurrent.futures.ThreadPoolExecutor(
-                    max_workers=workers)
+                max_workers=workers)
+
             # set up subreddit fetchers
-            for sub in config.keys():
-                self.fetch_subreddit(sub)
-            return
+            cur_time = time.time()
+            for channel in config.keys():
+                multireddit = config[channel]
+                add_to_dict(self.chan_map, multireddit, channel)
+                self.time_map[multireddit] = cur_time
+                for sub in multireddit.split('+'):
+                    add_to_dict(self.sub_map, sub, multireddit)
+                self.fetch_subreddit(multireddit)
 
 
-    async def fetch_reddit_json(self, type, id=None, link=None):
+    async def fetch_reddit_json(self, thing_type, thing_id=None, link=None):
         """
         Retrieves a reddit JSON object based on the type and unique ID.
 
@@ -67,22 +84,22 @@ class Reddit(object):
         :param str id: ID of reddit object.
         :param str link: Full Reddit link.
         """
-        if type == "t3":
-            url = "https://www.reddit.com/api/info.json?id={}_{}".format(type, id)
-        elif type == "t1":
+        if thing_type == "t3":
+            url = "https://www.reddit.com/api/info.json?id={}_{}".format(thing_type, thing_id)
+        elif thing_type == "t1":
             url = link + "/.json"
 
         r = await self.session.get(url, headers=self.headers)
         j = await r.json()
 
-        if type == "t1":
+        if thing_type == "t1":
             return j[1]['data']['children'][0]['data']
-        elif type == "t3":
+        elif thing_type == "t3":
             return j['data']['children'][0]['data']
 
 
     @irc3.event(r'.* PRIVMSG (?P<target>\S+) '
-            r':(?P<msg>.*https?://(redd.it/|.*\.reddit\.com/).*)')
+                r':(?P<msg>.*https?://(redd.it/|.*\.reddit\.com/).*)')
     async def on_reddit_link(self, target=None, msg=None, **kw):
         """
         When a reddit link matching the event regex is sent in the channel,
@@ -111,7 +128,7 @@ class Reddit(object):
                     gilded=c['gilded'],
                     upvotes=c['ups']))
             else:
-                c = await self.fetch_reddit_json('t3', id=match['link_id'])
+                c = await self.fetch_reddit_json('t3', thing_id=match['link_id'])
                 nsfw = " \x037\x02|\x02\x03 \x02NSFW\x02" if c['over_18'] else ""
                 self.bot.privmsg(target, self.link_template.format(
                     sub=c['subreddit'],
@@ -127,7 +144,7 @@ class Reddit(object):
 
            %%r <subreddit> [(hour|day|week|month|year|all)]
         """
-        return (await self.reddit(mask, target, args))
+        return await self.reddit(mask, target, args)
 
 
     @command(permission='view')
@@ -137,6 +154,9 @@ class Reddit(object):
            %%reddit <subreddit> [(hour|day|week|month|year|all)]
         """
         def find_time(args):
+            """
+            I was too lazy to write a lambda
+            """
             sorts = ['hour', 'day', 'week', 'month', 'year', 'all']
             for k in sorts:
                 if args[k] is True:
@@ -144,6 +164,10 @@ class Reddit(object):
             return None
 
         def get_top(j):
+            """
+            Get the top submission (excluding stickied posts) from a subreddit
+            listing.
+            """
             try:
                 if len(j['data']['children']) < 1:
                     return None
@@ -156,13 +180,12 @@ class Reddit(object):
             except KeyError:
                 return None
 
-
         sub = args['<subreddit>']
-        time = find_time(args)
-        if time is None:
+        timespan = find_time(args)
+        if timespan is None:
             url = "https://www.reddit.com/r/{}/.json".format(sub)
         else:
-            url = "https://www.reddit.com/r/{}/top/.json?t={}".format(sub, time)
+            url = "https://www.reddit.com/r/{}/top/.json?t={}".format(sub, timespan)
 
         r = await self.session.get(url, headers=self.headers)
         j = await r.json()
@@ -187,56 +210,67 @@ class Reddit(object):
         submission_stream generator.
 
         @returns c The PRAW submission object representing the newest submission
-        @param gen The submission_stream generator
+        @param (c, gen) A tuple containing the PRAW result and the multireddit it came from
         """
-        c = next(gen)
-        return c
+        while True:
+            try:
+                c = next(gen)
+                last_target_time = self.time_map[self.generator_map.inv[gen]]
+                if c.created > last_target_time:
+                    self.time_map[self.generator_map.inv[gen]] = c.created
+                    break
+            except Exception as e:
+                self.log.warning('Exception when retrieving reddit post: %s - %s', type(e).__name__, e)
+                return (None, self.generator_map.inv[gen])
+        return (c, self.generator_map.inv[gen])
 
 
-    def print_stream_result(self, c):
+    def print_stream_result(self, c, targets):
         """
         Callback function to print a new Reddit submission to all the channels
         subscribed for that subreddit.
 
         :param dict c: PRAW submission object
+        :param list targets: List of channels to output to
         """
         kw = {}
         kw['sub'] = sub = c.subreddit.display_name.lower()
         kw['title'] = c.title
         kw['author'] = c.author.name
         kw['id'] = c.id
-        targets = self.config[sub]
-        if isinstance(targets, str):
-            self.bot.privmsg(targets, self.new_template.format(**kw))
-        else:
-            for t in targets:
-                self.bot.privmsg(t, self.new_template.format(**kw))
+        self.log.debug('outputting to ' + str(targets))
+        for t in targets: # should always be a list
+            self.bot.privmsg('#' + t, self.new_template.format(**kw))
 
 
     async def run_stream(self, gen):
         """
         Coroutine to schedule the next new submission read off of the generator.
 
-        @param gen The generator to schedule
+        :param gen The generator to schedule
         """
         res = await self.bot.loop.run_in_executor(self.executor, self.fetch_post, gen)
-        self.print_stream_result(res)
-        sub = res.subreddit.display_name.lower()
-        asyncio.ensure_future(self.run_stream(self.subreddit_generators[sub]))
+        post = res[0]
+        if post is not None:
+            targets = self.chan_map[self.generator_map.inv[gen]]
+            self.print_stream_result(post, targets)
+
+        multi = res[1]
+        asyncio.ensure_future(self.run_stream(self.generator_map[multi]))
 
 
     def fetch_subreddit(self, subreddit):
         """
         Sets up the submission fetching loop for the subreddit passed in.
 
-        @param subreddit The subreddit to pass in
+        :param subreddit The subreddit to pass in
+        :param chan The channel to output results to
         """
         if self is None or subreddit is None:
             return
 
-        gen = praw.helpers.submission_stream(
-                self.praw, subreddit, limit=1, verbosity=0)
-        next(gen) # discard first entry
-        self.subreddit_generators[subreddit] = gen
+        gen = praw.helpers.submission_stream(self.praw, subreddit, limit=1, verbosity=0)
+        next(gen) # discard first submission, we only want new ones
+        self.generator_map[subreddit] = gen
         asyncio.ensure_future(self.run_stream(gen))
 
